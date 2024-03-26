5834dc3ddd66860c42b7322e73234b4f6db7f1b9
asm2212 <asmareadmasu0@gmail.com>
2024-03-26 21:01:40 +0300

    controller functions for user signup and login

--- budget-management/backend/controllers/authController.js
+++ budget-management/backend/controllers/authController.js
@@ -23,3 +23,32 @@ async function signup(req, res, next) {
         
     }
 }
+
+async function login(req, res, next) {
+    const { username, password } = req.body;
+
+    try {
+         const user = await User.findOne({ username });
+         if (!user) {
+             return res.status(400).json({ message: 'Username does not exist' });
+         }
+
+          const passwordMatch = await bcrypt.compare(password, user.password);
+          if (!passwordMatch) {
+              return res.status(400).json({ message: 'Password does not match' });
+          }
+          const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, 
+            { expiresIn: '1h' });
+
+            res.status(200).json({ token: token});
+
+
+    } catch (error) {
+        console.log("Error in Login", error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+    }
+    module.exports = {
+        signup,
+        login
+    }
