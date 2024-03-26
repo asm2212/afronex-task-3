e2c4ac6336c52c56e71217d70af9574a366c3f06
asm2212 <asmareadmasu0@gmail.com>
2024-03-26 11:10:54 +0300

    connect to mongodb

--- budget-management/index.js
+++ budget-management/index.js
@@ -5,24 +5,12 @@ const app = express();
 
 dotenv.config();
 
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Failed to connect to MongoDB', err));
 
-
 app.use(express.json());
 
-// Define your routes
-// Example: app.use('/api/auth', require('./routes/auth'));
-// Example: app.use('/api/users', require('./routes/users'));
-// Example: app.use('/api/expenses', require('./routes/expenses'));
-// Example: app.use('/api/admin', require('./routes/admin'));
-
-// Start the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
