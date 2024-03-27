93d1045d0bc405d1c8352a7b77ec6963df8e7ef1
asm2212 <asmareadmasu0@gmail.com>
2024-03-28 02:09:23 +0300

    budget controllers

--- budget-management/backend/routes/User.js
+++ budget-management/backend/routes/User.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authenticateToken = require('../middlewares/authMiddleware.js');
-const usersController = require('../controllers/userController.js');
+const usersController = require('../controllers/budgetController.js');
 
 router.get('/', authenticateToken, usersController.getAllUsers);
 router.get('/:id', authenticateToken, usersController.getUserById);
