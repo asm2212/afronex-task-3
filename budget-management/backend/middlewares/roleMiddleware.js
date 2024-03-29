1bac182ff4fde8c6512f0ef0d9e006c4e2aba3c5
asm2212 <asmareadmasu0@gmail.com>
2024-03-29 05:52:54 +0300

    checkRole middleware

---
+++ budget-management/backend/middlewares/roleMiddleware.js
@@ -0,0 +1,10 @@
+const checkRole = (role) => {
+    return (req, res, next) => {
+      if(req.user.role !== role) {
+        return res.status(403).json({ error: 'Forbidden'});
+    }
+    next();
+  };
+};
+
+module.exports = checkRole;
