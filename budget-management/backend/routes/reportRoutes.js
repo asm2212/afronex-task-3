765ae420608f669e0086a7471bfdadc96f3f7b8e
asm2212 <asmareadmasu0@gmail.com>
2024-03-30 07:58:18 +0300

    update report service

--- budget-management/backend/routes/reportRoutes.js
+++ budget-management/backend/routes/reportRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const authenticateUser = require('../middlewares/authMiddleware.js');
 const reportServices = require('../services/reportServices.js');
+const checkRole = require('../middlewares/checkRole.js');
 
 router.use(authenticateUser);
 
 router.get('/budgetReport',reportServices.generateBudgetReport);
-router.get('/expenseReport',reportServices.generateExpenseReport);
+router.get('/expenseReport',checkRole('admin'),reportServices.generateExpenseReport);
 
 module.exports = router;
