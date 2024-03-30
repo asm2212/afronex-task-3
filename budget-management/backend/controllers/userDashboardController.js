f4789444eb8a84420f9d0accd2d2c9176c620e52
asm2212 <asmareadmasu0@gmail.com>
2024-03-30 11:08:30 +0300

    Get user's total expenses

--- budget-management/backend/controllers/userDashboardController.js
+++ budget-management/backend/controllers/userDashboardController.js
@@ -1,3 +1,8 @@
+const User = require('../models/User.js');
+const BudgetCategory = require('../models/BudgetCategory.js');
+const Expense = require('../models/Expense.js');
+
+
 exports.getBudgetCategories = async (req, res) => {
     try {
       const user = await User.findById(req.user.id);
@@ -10,3 +15,21 @@ exports.getBudgetCategories = async (req, res) => {
       res.status(500).send("Server error");
     }
   };
+
+
+  exports.getTotalExpenses = async(req,res) => {
+    try {
+      const user = await User.findById(req.user.id);
+      if (!user) {
+        return res.status(404).send("User not found");
+      }
+      const expenses = await Expense.find({ user : user._id});
+      const totalExpenses = expenses.reduce((total,expense) => total + expense.amount ,0 );
+      res.json(totalExpenses);
+    
+    } catch (error) {
+      res.status(500).send("Server error");
+    }
+  };
+
+  
