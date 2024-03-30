8f5f2bfe28b6b7b10877059b6ee8b0f5abe9b7fe
asm2212 <asmareadmasu0@gmail.com>
2024-03-30 11:34:00 +0300

    Generate a report for user's expenses

--- budget-management/backend/controllers/userDashboardController.js
+++ budget-management/backend/controllers/userDashboardController.js
@@ -32,4 +32,41 @@ exports.getBudgetCategories = async (req, res) => {
     }
   };
 
-  
+
+exports.generateExpenseReport = async(req,res)=>{
+    try{
+        const user = await User.findById(req.user.id);
+        if(!user){
+            return res.status(404).send("User not found");
+        }
+        const expenses = await Expense.find({ user : user._id});
+
+        const totalExpenses = expenses.reduce((total,expense) => total + expense.amount ,0 );
+        const expenseCategories = {};
+
+        expense.forEach((expense ) => {
+            const categoryName = expense.categor.name;
+            if(!expenseCategories[categoryName]){
+                expenseCategories[categoryName] = expense.amount;
+            }else{
+                expenseCategories[categoryName] += expense.amount;
+            }
+
+        });
+
+        const report = {
+            totalExpenses: totalExpenses,
+            expenseCategories: expenseCategories,
+            numberOfExpenses: expenses.length,
+        };
+        res.json({report});
+        
+
+    }catch(error){
+        res.status(500).send("Server error");
+
+
+    }
+
+};
+
