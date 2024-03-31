431309c9a7681e1cf8cc0587e61f89e1e284e818
asm2212 <asmareadmasu0@gmail.com>
2024-03-31 08:50:18 +0300

    components

--- budget-management/backend/app.js
+++ budget-management/backend/app.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const errorHandler = require('./utils/errorHandler');
+const userDashboardRoutes = require('./routes/userDashboardRoutes.js');
 
-// Initialize Express app
+
 const app = express();
 
-// Connect to MongoDB
+
 connectDB();
 
-// Middleware
 app.use(express.json());
 
 // Routes
@@ -17,8 +17,14 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/budget', require('./routes/budgetRoutes'));
 app.use('/api/expense', require('./routes/expenseRoutes'));
 app.use('/api/report', require('./routes/reportRoutes'));
+app.use('/user-dashboard', userDashboardRoutes);
 
 // Error handling middleware
 app.use(errorHandler);
 
 module.exports = app;
+
+
+
+
+
--- budget-management/backend/routes/budgetRoutes.js
+++ budget-management/backend/routes/budgetRoutes.js
@@ -32,7 +32,7 @@ router.post('/categories', async (req, res) => {
     res.status(400).json(result);
   }
 });
-
+/* 
 router.put('/categories/:id', async (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
@@ -44,9 +44,22 @@ router.put('/categories/:id', async (req, res) => {
   } else {
     res.status(404).json(result);
   }
-});
+}); */
 
-router.delete('/categories/:id', async (req, res) => {
+router.put('/categories/:id',async(req,res) => {
+    const {id} = req.params.id;
+    const {name} = req.body;
+
+    const result = await budgetService.updateCategory(id,name);
+
+    if(result.success){
+        res.status(200).json({message:result.message});
+    } else{
+        res.status(404).json({message:'error'});
+    }
+})
+
+/* router.delete('/categories/:id', async (req, res) => {
   const { id } = req.params;
 
   const result = await budgetService.removeBudgetCategory(id);
@@ -56,6 +69,14 @@ router.delete('/categories/:id', async (req, res) => {
   } else {
     res.status(404).json(result);
   }
-});
+}); */
+
+router.delete('/categories/:id',async(req,res) => {
+    if(result.success){
+        res.status(200).json({message: "Categories deleted"});
+    } else {
+        res.status(404).json({message: "server error"});
+    }
+})
 
 module.exports = router;
